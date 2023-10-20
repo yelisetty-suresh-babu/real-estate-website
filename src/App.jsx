2e4fa5fbb3d5f26c3b90a80d64ee46270466d843
yelisetty-suresh-babu <yelisettysuresh2@gmail.com>
2023-10-20 16:34:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="h-full w-full items-center justify-center">
+        <div className="bg-red-500 h-32 w-32">
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
