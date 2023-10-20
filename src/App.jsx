2a85c8837a23b5ec53df951707e4dca88f5cd663
yelisetty-suresh-babu <yelisettysuresh2@gmail.com>
2023-10-20 16:51:07 +0530

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import signIn from "./pages/signIn";
+import signUp from "./pages/signUp";
 
 function App() {
   return (
-    <div className="h-full w-full items-center justify-center">
-        <div className="bg-red-500 h-32 w-32">
-
-        </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<signIn />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/signUp" element={<signUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Profile() {
+  return <>Profiel</>;
+}
+
+export default Profile;
